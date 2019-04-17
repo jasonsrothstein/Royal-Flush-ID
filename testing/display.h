ffec050fc644d95c7f4ca10069c774af2a933698
Matthew Cunningham <mcunningham1519@gmail.com>
2019-04-17 09:28:20 -0400

    Create display.h

---
+++ testing/display.h
@@ -0,0 +1,17 @@
+/*
+ * display.h
+ *
+ *  Created on: Apr 17, 2019
+ *      Author: cunnin66
+ */
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+void displayX(int, int, char*);
+void display(int, char*, char);
+void clear_display(void);
+void lcd_init(void);
+
+
+#endif /* DISPLAY_H_ */
